c886b5e09fa1474552b56e957d1d8b75aa1d4104
vshaonian <1251602300@qq.com>
2016-09-13 22:55:32 +0800

    my jstask
    js

---
+++ task1.js
@@ -0,0 +1,24 @@
+function changeColor(){
+    var lattice=document.getElementsByTagName('div')
+    var latticeNum=Math.floor(Math.random()*9)
+    var colorNum=Math.floor(Math.random()*3)
+    switch(colorNum){
+        case 0:
+        color="red";
+        colorName="红色";
+        break;
+
+        case 1:
+        color="green";
+        colorName="绿色";
+        break;
+
+        case 2:
+        color="blue";
+        colorName="蓝色";
+        break;
+    }
+    lattice[latticeNum].style.backgroundColor=color
+    console.log("格子"+(latticeNum+1)+"变成了"+colorName)
+}
+setInterval(changeColor,1000)

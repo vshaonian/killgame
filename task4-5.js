0182f5ac1ea3f30c9d87641243f175a4ad27abe2
vshaonian <1251602300@qq.com>
2017-01-15 18:12:06 +0800

    修复玩家与角色不对应

--- task4-5.js
+++ task4-5.js
@@ -3,9 +3,11 @@
  */
 var deadPlayers = JSON.parse(localStorage.deadPlayerArr);
 console.log(deadPlayers);
-for(var i = 0; i < deadPlayers.length; i++) {
-
-}
+var cha = JSON.parse(localStorage.ids);
+console.log(cha);
+// for(var i = 0; i < deadPlayers.length; i++) {
+//
+// }
 
 //输出水民人数
 
@@ -22,10 +24,10 @@ console.log(peoples);
 
 //输出过程
 
-var days = Math.floor(deadPlayers.length/2)+1;
-var main = document.getElementsByClassName("mainbody")[0]
+//var days = Math.floor(deadPlayers.length/2)+1;
+var main = document.getElementsByClassName("mainbody")[0];
 
-for(var j = 0; j < days; j++) {
+for(var j = 0; j < deadPlayers.length; j++) {
     var div = document.createElement("div");
     div.innerHTML = '<p>' +
         '<span class="day">第' + (j+1) + '天</span>' +
@@ -33,20 +35,28 @@ for(var j = 0; j < days; j++) {
         '</p>' +
         '<p class="content-txt">' +
         '晚上：' +
-        '<span>5<span>' +
+        '<span>' + deadPlayers[j] + '<span>' +
         '号玩家被杀手杀死，' +
-        '<span>5</span>' +
-        '号是水民' +
+        '<span>'+ deadPlayers[j] +'</span>' +
+        '号是' + cha[deadPlayers[j]-1] +
         '</p>' +
         '<p class="content-txt">' +
         '白天：' +
-        '<span>8</span>' +
+        '<span>' + deadPlayers[j+1] + '</span>' +
         '号被全民投票投死，' +
-        '<span>8</span>' +
-        '号是杀手' +
+        '<span>'+ deadPlayers[j+1] +'</span>' +
+        '号是' +
+        cha[deadPlayers[j+1]-1] +
         '</p>'
     div.setAttribute("class","day-rel");
     main.appendChild(div);
+    j++;
 }
 
-console.log(main);
+//
+
+var content_txt = document.getElementsByClassName("content-txt")
+var last_content = content_txt[content_txt.length-1];
+if(deadPlayers.length % 2 !== 0) {
+    last_content.style.display = "none";
+}
